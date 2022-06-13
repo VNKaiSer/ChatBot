696fd9962312287093d23cfac2550022d4ec38e1
unknown <tandatvok16@gmail.com>
2022-06-13 15:24:58 +0700

    update

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const VERIFY_TOKEN = process.env.VERIFY_TOKEN
 let getHomePage = (req, res) => {
     return res.send("Hello Word")
 };
@@ -14,8 +16,13 @@ let postWebhook = (req, res) => {
 
             // Gets the message. entry.messaging is an array, but 
             // will only ever contain one message, so we get index 0
+            // Gets the body of the webhook event
             let webhook_event = entry.messaging[0];
             console.log(webhook_event);
+
+            // Get the sender PSID
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid);
         });
 
         // Returns a '200 OK' response to all requests
@@ -27,7 +34,7 @@ let postWebhook = (req, res) => {
 };
 
 let getWebhook = (req, res) => {
-    let VERIFY_TOKEN = process.env.VERIFY_TOKEN
+
 
     // Parse the query params
     let mode = req.query['hub.mode'];
@@ -50,6 +57,21 @@ let getWebhook = (req, res) => {
         }
     }
 };
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+
+}
 
 module.exports = {
     getHomePage: getHomePage, // key : values
